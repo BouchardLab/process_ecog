5a132f8fdffc9df393ac97d9ed6dec5304bc1b8e
JesseLivezey <jesse.livezey@gmail.com>
2015-09-08 21:44:10 -0700

    blocks work

--- blocks/ecog_spearmint.py
+++ blocks/ecog_spearmint.py
@@ -6,26 +6,30 @@ from load_data import pylearn2_ecog
 
 
 def main(job_id, opt_params):
-    audio, batch_size, folds, init_params = ecog_static_params()
-    opt_params = filter_params({})
+    # Load parameters for dataset
+    ds_params, static_params = ecog_static_params()
+    # Set defaults
+    opt_params = filter_params(opt_params)
+    folds = static_params['folds']
 
     train_misclass = np.zeros(folds)
     valid_misclass = np.zeros(folds)
     test_misclass = np.zeros(folds)
 
     for ii in range(folds):
-        static_params = init_params.copy()
-        static_params['fold'] = ii
-        data = pylearn2_ecog(audio, batch_size, static_params)
-        tr, va, te = train_model(data, opt_params, static_params)
+        ds_params['fold'] = ii
+        data = pylearn2_ecog(ds_params, static_params)
+        tr, va, te = train_model(job_id, data, opt_params, static_params)
         train_misclass[ii] = tr
         valid_misclass[ii] = va
         test_misclass[ii] = te
 
     print '----------result------------'
-    print 'mean', valid_misclass.mean()
-    print 'std', valid_misclass.std()
+    print 'train mean', train_misclass.mean()
+    print 'train std', train_misclass.std()
+    print 'valid mean', valid_misclass.mean()
+    print 'valid std', valid_misclass.std()
 
 
 if __name__ == "__main__":
-    main(None, None)
+    main(0, {})
--- blocks/load_data.py
+++ blocks/load_data.py
@@ -6,15 +6,17 @@ from fuel.schemes import SequentialScheme
 
 from pylearn2.datasets import ecog
 
-def pylearn2_ecog(audio, batch_size, kwargs):
+def pylearn2_ecog(ds_params, static_params):
     splits = ['train', 'valid', 'test']
     datasets = []
+    audio = static_params['audio']
+    batch_size = static_params['monitor_batch_size']
     if audio:
         raise NotImplementedError
     else:
         data_file = '${PYLEARN2_DATA_PATH}/ecog/EC2_CV_85_nobaseline_aug.h5'
     for sp in splits:
-        cur_dataset = ecog.ECoG(data_file, sp, **kwargs)
+        cur_dataset = ecog.ECoG(data_file, sp, **ds_params)
         ds_x = np.squeeze(cur_dataset.get_topological_view())
         ds_y = cur_dataset.y.argmax(axis=1).astype(np.int32)
         d = {'x': ds_x,
