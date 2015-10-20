7dc5ef1f231ec3f717198de3f8fbef87c1568d4f
afbujan <afbujan@berkeley.edu>
2015-10-20 15:08:47 -0700

    added script to turn hdf5-refs into token names and ids

---
+++ ecog/preprocessing_code/refs2strings.py
@@ -0,0 +1,18 @@
+import h5py
+
+def get_token_ids(filename):
+    """
+    Extracts tokens and ids from Ben's mat files
+    Arguments
+    ---------
+    filename: string
+            filename (with path if not in dir)
+    Outputs
+    -------
+    token_ids: list of tuples
+            token names and ids
+    """
+    f = h5py.File(filename,'r')
+    get_token = lambda ref: f[ref].value.tostring().decode("utf-16")
+    return zip(map(get_token,f['out/slist'][0]),np.unique(f['out/labs'].value.astype('int')))
+
