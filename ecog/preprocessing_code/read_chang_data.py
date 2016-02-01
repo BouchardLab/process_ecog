2347975761ba2a85c2361f594f51b72af2432af0
Jesse Livezey <jesse.livezey@berkeley.edu>
2016-01-31 23:07:04 -0800

    textgrid parsing works for ec9

--- ecog/preprocessing_code/read_chang_data.py
+++ ecog/preprocessing_code/read_chang_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 __author__ = 'David Conant, Jesse Livezey'
 
+import sys
 import argparse, h5py, re, os, glob, csv
 import numpy as np
 import scipy as sp
@@ -8,10 +9,10 @@ import scipy.stats as stats
 from scipy.io import loadmat
 import pandas as pd
 
-import HTK
+from utils import HTK, transcripts
 
 
-def htk_to_hdf5(path, blocks, task, align_window=None, data_type='HG', baseline='whole'):
+def htk_to_hdf5(path, blocks, task, align_window=None, align_pos = 0, data_type='HG', baseline='whole'):
     """
     Process task data into segments with labels.
 
@@ -25,6 +26,8 @@ def htk_to_hdf5(path, blocks, task, align_window=None, data_type='HG', baseline=
         Type of tokens to be extracted.
     align_window : list of two ints
         Time window in seconds around each token.
+    align_pos : ints
+        Align to start of this phoneme.
     data_type : str
         Type of data to use.
 
@@ -69,52 +72,48 @@ def htk_to_hdf5(path, blocks, task, align_window=None, data_type='HG', baseline=
         rval = 'align_window_' + str(align_window[0]) + '_to_'+ str(align_window[1])
         return rval
 
-    folder, subject = os.path.split(path)
+    folder, subject = os.path.split(os.path.normpath(path))
     fname = os.path.join(path, 'hdf5', (subject + '_' + block_str(blocks)
                                        + task + '_' + data_type + '_'
                                        + align_window_str(align_window) + '.h5'))
 
-    if not os.path.isfile(fname):
-        D = dict((token, np.array([])) for token in tokens)
-        stop_times = dict((token, np.array([])) for token in tokens)
-        start_times = dict((token, np.array([])) for token in tokens)
-        for iblock, block in enumerate(blocks):
-            print 'Processing block ' + str(block)
-            blockname = subject + '_B' + str(block)
-            blockpath = os.path.join(path, blockname)
-            # Convert parseout to dataframe
-            textgrid_path = os.path.join(blockpath, blockname + '_transcription_final.TextGrid')
-            lab_path = os.path.join(blockpath, blockname + '_transcription_final.lab')
-            if os.path.isfile(textgrid_path):
-                parseout = parseTextGrid(textgrid_path)
-            elif os.path.isfile(lab_path):
-                parseout = parseLab(lab_path)
-            else:
-                raise ValueError("Transcription not found at: "
-                                 + str(textgrid_path) + " or: "
-                                 + str(lab_path))
+    D = dict((token, np.array([])) for token in tokens)
+    stop_times = dict((token, np.array([])) for token in tokens)
+    start_times = dict((token, np.array([])) for token in tokens)
+    for iblock, block in enumerate(blocks):
+        print 'Processing block ' + str(block)
+        blockname = subject + '_B' + str(block)
+        blockpath = os.path.join(path, blockname)
+        # Convert parseout to dataframe
+        textgrid_path = os.path.join(blockpath, blockname + '_transcription_final.TextGrid')
+        lab_path = os.path.join(blockpath, blockname + '_transcription_final.lab')
+        if os.path.isfile(textgrid_path):
+            parseout = transcripts.parse_final_TextGrid(textgrid_path)
+        elif os.path.isfile(lab_path):
+            parseout = transcripts.parse_Lab(lab_path)
+        else:
+            raise ValueError("Transcription not found at: "
+                             + str(textgrid_path) + " or: "
+                             + str(lab_path))
 
-            df = make_df(parseout, block, subject)
+        df = make_df(parseout, block, subject)
 
-            for ind, token in enumerate(tokens):
-                match = [token in t for t in df['label']]
-                event_times = df['start'][match & (df['mode'] == 'speak')]
-                stop = df['stop'][match & (df['mode'] == 'speak')].values
-                start = df['start'][match & (df['mode'] == 'speak')].values
+        for ind, token in enumerate(tokens):
+            match = [token in t for t in df['label']]
+            event_times = df['start'][match & (df['mode'] == 'speak')]
+            stop = df['stop'][match & (df['mode'] == 'speak')].values
+            start = df['start'][match & (df['mode'] == 'speak')].values
 
-                stop_times[token] = (np.hstack((stop_times[token], stop.astype(float))) if 
-                                     stop_times[token].size else stop.astype(float))
-                start_times[token] = (np.hstack((start_times[token], start.astype(float))) if
-                                      start_times[token].size else start.astype(float))
-                D[token] = (np.dstack((D[token], run_makeD(blockpath, event_times,
-                                                           align_window, dt=data_type))) if 
-                            D[token].size else run_makeD(blockpath, event_times, align_window, dt=data_type))
+            stop_times[token] = (np.hstack((stop_times[token], stop.astype(float))) if 
+                                 stop_times[token].size else stop.astype(float))
+            start_times[token] = (np.hstack((start_times[token], start.astype(float))) if
+                                  start_times[token].size else start.astype(float))
+            D[token] = (np.dstack((D[token], run_makeD(blockpath, event_times,
+                                                       align_window, dt=data_type))) if 
+                        D[token].size else run_makeD(blockpath, event_times, align_window, dt=data_type))
 
-        print('Saving to: '+fname)
-        save_hdf5(fname, D, tokens)
-    else:
-        print 'File found; Loading...'
-        D, tokens = load_hdf5(fname)
+    print('Saving to: '+fname)
+    save_hdf5(fname, D, tokens)
 
     anat = load_anatomy(path)
     return (D, anat, start_times, stop_times)
@@ -146,8 +145,6 @@ def save_hdf5(fname, D, tokens):
         f.create_dataset('y', data=y)
         f.create_dataset('tokens', data=tokens)
 
-def load_hdf5(fname):
-    raise NotImplementedError
 
 def make_df(parseout, block, subject):
     keys = sorted(parseout.keys())
@@ -247,7 +244,6 @@ def makeD(data, fs_data, times, align_window=None, bad_times=None, bad_electrode
 
     return D
 
-
 def load_HG(blockpath):
     htk_path = os.path.join(blockpath, 'HilbAA_70to150_8band')
     HTKout = HTK.readHTKs(htk_path)
@@ -264,153 +260,6 @@ def loadForm(blockpath):
             F.append(column)
     F = np.array(F)
     return F
-
-def parseTextGrid(fname):
-    """
-    Reads in a TextGrid (used by Praat) and returns a dictionary with the events
-    contained within, as well as their times, labels, and hierarchy.
-    Assumes a 2 tier textgrid corresponding to words and phonemes
-
-    Parameters:
-    fname: filename of the TextGrid
-
-    Returns:
-    events (a dictionary) with keys:
-        label: an array of strings identifying each event according to the utterance
-        start: an array of the start times for each event
-        stop: an array of the stop times for each event
-        tier: an array specifying the tier (phoneme or word) for each event
-        contains: an array specifying the phonemes contained within each event
-        contained_by: an array specifying the words contiaining each event
-        position: the position of each phoneme within the word
-    """
-
-    with open(fname) as tg:
-        content = tg.readlines()
-
-    label = []
-    start = []
-    stop = []
-    tier = []
-
-
-    t = 0
-    c = -1
-    tiers = ['phoneme', 'word', 'phrase']
-    #Loop through each line of the text file
-    for line in content:
-        c = c + 1
-        if 'item [2]:' in line: #iterate tier as they pass
-            t = 1
-        if 'item [3]:' in line:
-            t = 2
-        if 'text =' in line:
-            if line[20:-3] == 'sp' or not line[20:-3]:
-                continue
-
-            label.append(line[20:-3])
-            start.append(float(content[c-2][19:-2]))
-            stop.append(float(content[c-1][19:-2]))
-            tier.append(tiers[t])
-    label = np.array(label)
-    start = np.array(start)
-    stop = np.array(stop)
-    tier = np.array(tier)
-
-    phones = np.where(tier == 'phoneme')[0]
-    words = np.where(tier == 'word')[0]
-
-    contained_by = [-1]*label.size
-    contains = [-1]*label.size
-    position = np.ones(label.size)*-1
-
-
-    #Determine hierarchy of events
-    for ind in words:
-        if t == 1: #If no phrase tier, word is highest tier
-            position[ind] = 1
-            contained_by[ind] = -1;
-
-        #Find contained phonemes
-        lower = start[ind] - 0.01
-        upper = stop[ind] + 0.01
-        startCandidates = np.where(start >= lower)[0]
-        stopCandidates = np.where(stop <= upper)[0]
-        intersect = np.intersect1d(startCandidates,stopCandidates)
-        cont = list(intersect)
-        cont.remove(ind)
-        contains[ind] = cont
-        for i in cont:
-            contained_by[i] = ind
-
-
-    for ind in phones:
-        #Find phonemes in the same word, position is order in list
-        sameWord = np.where(np.asarray(contained_by) == contained_by[ind])[0]
-        position[ind] = np.where(sameWord == ind)[0] + 1
-
-
-    contains = np.asarray(contains, dtype=object)
-    contained_by = np.asarray(contained_by, dtype=object)
-
-    events = {'label': label, 'start': start, 'stop': stop,
-              'tier': tier, 'contains': contains,
-              'contained_by': contained_by, 'position': position}
-    return events
-
-
-def parseLab(fname):
-    """
-    Reads a 'lab' transcript and returns a dictionary with the events
-    contained within, as well as their times, labels, and hierarchy.
-
-    Parameters
-    ----------
-    fname: filename of the 'lab' transcript
-
-    Returns:
-    events (a dictionary) with keys:
-        label: an array of strings identifying each event according to the utterance
-        start: an array of the start times for each event
-        stop: an array of the stop times for each event
-        tier: an array specifying the tier (phoneme or word) for each event
-        contains: an array specifying the phonemes contained within each event
-        contained_by: an array specifying the words contiaining each event
-        position: the position of each phoneme within the word
-    """
-    start = []
-    stop  = []
-    tier = []
-    position = []
-    contains = []
-    contained_by = []
-    label = []
-    with open(fname) as lab:
-        content = lab.readlines()
-
-    for line in content:
-        token = "".join(re.findall("[a-z]",line)+[line[-2]])
-        isplosive = token[0] in ['d','t','b','p','g','k','gh']
-        if (isplosive and '4' in token) or (not isplosive and '3' in token):
-            start.append(line[0:line.find(' ')])
-            stop.append(line[line.find(' ')+1:line.find(' ',line.find(' ')+1)])
-            start[-1] = float(start[-1])/1E7
-            stop[-1] = float(stop[-1])/1E7
-            tier.append('word')
-            position.append(-1)
-            contains.append(-1)
-            contained_by.append(-1)
-            #label.append(token)
-            label.append(token[:-1] + '2') #For textgrid convention
-    label = np.array(label)
-    start = np.array(start)
-    stop = np.array(stop)
-    tier = np.array(tier)
-    contains = np.asarray(contains, dtype=object)
-    contained_by = np.asarray(contained_by, dtype=object)
-    events = {'label': label, 'start': start, 'stop': stop, 'tier': tier,
-              'contains': contains, 'contained_by': contained_by, 'position': position}
-    return events
 
 def load_anatomy(subj_dir):
     anatomy_filename = glob.glob(os.path.join(subj_dir, '*_anat.mat'))
@@ -493,9 +342,10 @@ if __name__ == "__main__":
     parser.add_argument('path', help='path to subject folder')
     parser.add_argument('blocks', nargs='+', type=int)
     parser.add_argument('-t', '--task', default='CV')
-    parser.add_argument('-a', '--align_window', nargs=2, type=float, default=[-1., 1.])
+    parser.add_argument('-a', '--align_window', nargs=2, type=float, default=[-.5, .79])
+    parser.add_argument('-p', '--align_pos', type=int, default=1)
     parser.add_argument('-d', '--data_type', default='HG')
     parser.add_argument('-l', '--baseline', default='whole')
     args = parser.parse_args()
     htk_to_hdf5(args.path, args.blocks, args.task, args.align_window,
-                args.data_type, args.baseline)
+                args.align_pos, args.data_type, args.baseline)
