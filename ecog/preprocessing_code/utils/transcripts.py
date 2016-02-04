7e63b7dbbb8285077ad9907c985b68a89c5a6134
Jesse Livezey <jesse.livezey@berkeley.edu>
2016-02-03 23:56:57 -0800

    more unified parsing

--- ecog/preprocessing_code/utils/transcripts.py
+++ ecog/preprocessing_code/utils/transcripts.py
@@ -4,104 +4,6 @@ import re
 import numpy as np
 
 
-def parse_final_TextGrid(fname):
-    """
-    Reads in a TextGrid (used by Praat) and returns a dictionary with the events
-    contained within, as well as their times, labels, and hierarchy.
-    Assumes a 2 tier textgrid corresponding to words and phonemes
-
-    Parameters:
-    fname: filename of the TextGrid
-
-    Returns:
-    events (a dictionary) with keys:
-        label: an array of strings identifying each event according to the utterance
-        start: an array of the start times for each event
-        stop: an array of the stop times for each event
-        tier: an array specifying the tier (phoneme or word) for each event
-        contains: an array specifying the phonemes contained within each event
-        contained_by: an array specifying the words contiaining each event
-        position: the position of each phoneme within the word
-    """
-
-    with open(fname) as tg:
-        content = tg.readlines()
-
-    label = []
-    start = []
-    stop = []
-    tier = []
-
-
-    t = 0
-    content = content[7:]
-    #Loop through each line of the text file
-    for ii, line in enumerate(content):
-        if 'IntervalTier' in line:
-            continue
-        elif 'phone' in line:
-            t = 'phone'
-            continue
-        elif 'word' in line:
-            t = 'word'
-            continue
-        elif 'sp' in line:
-            continue
-        else:
-            try:
-                float(line)
-            except ValueError:
-                label.append(line[1:-1])
-                start.append(float(content[ii-2]))
-                stop.append(float(content[ii-1]))
-                tier.append(t)
-    label = np.array(label)
-    start = np.array(start)
-    stop = np.array(stop)
-    tier = np.array(tier)
-
-    phones = np.where(tier == 'phoneme')[0]
-    words = np.where(tier == 'word')[0]
-
-    contained_by = [-1]*label.size
-    contains = [-1]*label.size
-    position = np.ones(label.size)*-1
-
-
-    #Determine hierarchy of events
-    for ind in words:
-        if t == 1: #If no phrase tier, word is highest tier
-            position[ind] = 1
-            contained_by[ind] = -1;
-
-        #Find contained phonemes
-        lower = start[ind] - 0.01
-        upper = stop[ind] + 0.01
-        startCandidates = np.where(start >= lower)[0]
-        stopCandidates = np.where(stop <= upper)[0]
-        intersect = np.intersect1d(startCandidates,stopCandidates)
-        cont = list(intersect)
-        cont.remove(ind)
-        contains[ind] = cont
-        for i in cont:
-            contained_by[i] = ind
-
-
-    for ind in phones:
-        #Find phonemes in the same word, position is order in list
-        sameWord = np.where(np.asarray(contained_by) == contained_by[ind])[0]
-        position[ind] = np.where(sameWord == ind)[0] + 1
-
-
-    contains = np.asarray(contains, dtype=object)
-    contained_by = np.asarray(contained_by, dtype=object)
-
-    events = {'label': label, 'start': start, 'stop': stop,
-              'tier': tier, 'contains': contains,
-              'contained_by': contained_by, 'position': position}
-    return events
-
-
 def parse_TextGrid(fname):
     """
     Reads in a TextGrid (used by Praat) and returns a dictionary with the events
@@ -131,72 +33,45 @@ def parse_TextGrid(fname):
     tier = []
 
 
-    t = 0
-    c = -1
-    tiers = ['phoneme', 'word', 'phrase']
-    #Loop through each line of the text file
-    for line in content:
-        c = c + 1
-        if 'item [2]:' in line: #iterate tier as they pass
-            print '1'
-            t = 1
-        if 'item [3]:' in line:
-            print '2'
-            t = 2
-        if 'text =' in line:
-            if line[20:-3] == 'sp' or not line[20:-3]:
+    if any(['item [' in c for c in content]):
+        # Normal formatting
+        for ii, line in enumerate(content):
+            if 'item [1]:' in line: #iterate tier as they pass
+                t = 'phone'
+            elif 'item [2]:' in line: #iterate tier as they pass
+                t = 'word'
+            elif 'text =' in line:
+                if line[20:-3] == 'sp' or not line[20:-3]:
+                    continue
+
+                label.append(line[20:-3])
+                start.append(float(content[ii-2][19:-2]))
+                stop.append(float(content[ii-1][19:-2]))
+                tier.append(t)
+    else:
+        # Truncated formatting
+        content = content[7:]
+        for ii, line in enumerate(content):
+            if 'IntervalTier' in line:
                 continue
+            elif 'phone' in line:
+                t = 'phone'
+                continue
+            elif 'word' in line:
+                t = 'word'
+                continue
+            elif 'sp' in line:
+                continue
+            else:
+                try:
+                    float(line)
+                except ValueError:
+                    label.append(line[1:-1])
+                    start.append(float(content[ii-2]))
+                    stop.append(float(content[ii-1]))
+                    tier.append(t)
 
-            label.append(line[20:-3])
-            start.append(float(content[c-2][19:-2]))
-            stop.append(float(content[c-1][19:-2]))
-            tier.append(tiers[t])
-    label = np.array(label)
-    start = np.array(start)
-    stop = np.array(stop)
-    tier = np.array(tier)
-
-    phones = np.where(tier == 'phoneme')[0]
-    words = np.where(tier == 'word')[0]
-
-    contained_by = [-1]*label.size
-    contains = [-1]*label.size
-    position = np.ones(label.size)*-1
-
-
-    #Determine hierarchy of events
-    for ind in words:
-        if t == 1: #If no phrase tier, word is highest tier
-            position[ind] = 1
-            contained_by[ind] = -1;
-
-        #Find contained phonemes
-        lower = start[ind] - 0.01
-        upper = stop[ind] + 0.01
-        startCandidates = np.where(start >= lower)[0]
-        stopCandidates = np.where(stop <= upper)[0]
-        intersect = np.intersect1d(startCandidates,stopCandidates)
-        cont = list(intersect)
-        cont.remove(ind)
-        contains[ind] = cont
-        for i in cont:
-            contained_by[i] = ind
-
-
-    for ind in phones:
-        #Find phonemes in the same word, position is order in list
-        sameWord = np.where(np.asarray(contained_by) == contained_by[ind])[0]
-        position[ind] = np.where(sameWord == ind)[0] + 1
-
-
-    contains = np.asarray(contains, dtype=object)
-    contained_by = np.asarray(contained_by, dtype=object)
-
-    events = {'label': label, 'start': start, 'stop': stop,
-              'tier': tier, 'contains': contains,
-              'contained_by': contained_by, 'position': position}
-    return events
-
+    return format_events(label, start, stop, tier)
 
 def parse_Lab(fname):
     """
@@ -227,26 +102,74 @@ def parse_Lab(fname):
     with open(fname) as lab:
         content = lab.readlines()
 
-    for line in content:
-        token = "".join(re.findall("[a-z]",line)+[line[-2]])
-        isplosive = token[0] in ['d','t','b','p','g','k','gh']
+    for ii, line in enumerate(content):
+        _, time, token = line.split(' ')
+        isplosive = token[:-3] in ['d','t','b','p','g','k','gh']
         if (isplosive and '4' in token) or (not isplosive and '3' in token):
-            start.append(line[0:line.find(' ')])
-            stop.append(line[line.find(' ')+1:line.find(' ',line.find(' ')+1)])
-            start[-1] = float(start[-1])/1E7
-            stop[-1] = float(stop[-1])/1E7
+            _, start_time, start_token = content[ii-1].split(' ')
+            _, stop_time, stop_token = content[ii+1].split(' ')
+            # First phone
+            tier.append('phone')
+            start.append(start_time/1.e7)
+            stop.append(time/1.e7)
+            label.append(token[:-3]
+            # Second phone
+            tier.append('phone')
+            start.append(time/1.e7)
+            stop.append(stop_time/1.e7)
+            label.append(token[-3:-1]
+            # Word
             tier.append('word')
-            position.append(-1)
-            contains.append(-1)
-            contained_by.append(-1)
-            #label.append(token)
-            label.append(token[:-1] + '2') #For textgrid convention
+            start.append(time/1.e7)
+            stop.append(stop_time/1.e7)
+            # TextGrid 'speak' convention
+            label.append(token[:-1] + '2')
+
+    return format_events(label, start, stop, tier)
+
+def format_events(label, start, stop, tier):
     label = np.array(label)
     start = np.array(start)
     stop = np.array(stop)
     tier = np.array(tier)
+
+    phones = np.where(tier == 'phoneme')[0]
+    words = np.where(tier == 'word')[0]
+
+    contained_by = [-1]*label.size
+    contains = [-1]*label.size
+    position = np.ones(label.size)*-1
+
+
+    #Determine hierarchy of events
+    for ind in words:
+        if t == 1: #If no phrase tier, word is highest tier
+            position[ind] = 1
+            contained_by[ind] = -1;
+
+        #Find contained phonemes
+        lower = start[ind] - 0.01
+        upper = stop[ind] + 0.01
+        startCandidates = np.where(start >= lower)[0]
+        stopCandidates = np.where(stop <= upper)[0]
+        intersect = np.intersect1d(startCandidates,stopCandidates)
+        cont = list(intersect)
+        cont.remove(ind)
+        contains[ind] = cont
+        for i in cont:
+            contained_by[i] = ind
+
+
+    for ind in phones:
+        #Find phonemes in the same word, position is order in list
+        sameWord = np.where(np.asarray(contained_by) == contained_by[ind])[0]
+        position[ind] = np.where(sameWord == ind)[0] + 1
+
+
     contains = np.asarray(contains, dtype=object)
     contained_by = np.asarray(contained_by, dtype=object)
-    events = {'label': label, 'start': start, 'stop': stop, 'tier': tier,
-              'contains': contains, 'contained_by': contained_by, 'position': position}
+
+    events = {'label': label, 'start': start, 'stop': stop,
+              'tier': tier, 'contains': contains,
+              'contained_by': contained_by, 'position': position}
     return events
