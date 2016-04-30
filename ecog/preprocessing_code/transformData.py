b990f40e8ba68a433f19a9c9bca79e7b914d5570
Ben Dichter <ben.dichter@gmail.com>
2016-04-29 20:03:37 -0700

    calculate and save chang lab hilbert bands

--- ecog/preprocessing_code/transformData.py
+++ ecog/preprocessing_code/transformData.py
@@ -1,25 +1,71 @@
 from __future__ import division
+import os
 import numpy as np
 import h5py
-import HTK_hilb as htk
-reload(htk)
-from pyfftw.interfaces.numpy_fft import fft,ifft,fftfreq
+import glob
+#from pyfftw.interfaces.numpy_fft import fft,ifft,fftfreq
+from numpy.fft import fft, ifft, fftfreq
 from scipy.io import loadmat
 from optparse import OptionParser
 
-
-import downSampleEcog as dse
-reload(dse)
-import subtractCAR as scar
-reload(scar)
-import applyLineNoiseNotch as notch
-reload(notch)
-import applyHilbertTransform as aht
-reload(aht)
-import deleteBadTimeSegments as dbts
-reload(dbts)
+import ecog.preprocessing_code.HTK_hilb as htk
+import ecog.preprocessing_code.downSampleEcog as dse
+import ecog.preprocessing_code.subtractCAR as scar
+import ecog.preprocessing_code.applyLineNoiseNotch as notch
+import ecog.preprocessing_code.applyHilbertTransform as aht
+from tqdm import tqdm
+import ecog.preprocessing_code.deleteBadTimeSegments as dbts
 
 __authors__ = "Alex Bujan (adapted from Kris Bouchard)"
+
+
+def load_electrode_labels(subj_dir):
+    """
+    Get anatomy. Try newest format, and then progressively earlier formats.
+
+    :param subj_dir:
+    :return: np.array of labels as strings
+    """
+    anatomy_filename = glob.glob(os.path.join(subj_dir, '*_anat.mat'))
+    elect_labels_filename = glob.glob(os.path.join(subj_dir, 'elec_labels.mat'))
+    hd_grid_file = glob.glob(os.path.join(subj_dir, 'Imaging', 'elecs', 'hd_grid.mat'))
+    TDT_elecs_file = glob.glob(os.path.join(subj_dir, 'Imaging', 'elecs', 'TDT_elecs_all.mat'))
+
+    if TDT_elecs_file:
+        mat_in = loadmat(TDT_elecs_file[0])
+        try:
+            electrode_labels = mat_in['anatomy'][:, -1].ravel()
+            print('anatomy from TDT_elecs_all')
+            return np.array([a[0] for a in electrode_labels])
+        except:
+            pass
+
+    if hd_grid_file:
+        mat_in = loadmat(hd_grid_file[0])
+        if 'anatomy' in mat_in:
+            electrode_labels = np.concatenate(mat_in['anatomy'].ravel())
+            print('anatomy hd_grid')
+
+            return electrode_labels
+
+    if anatomy_filename:
+        anatomy = loadmat(anatomy_filename[0])
+        anat = anatomy['anatomy']
+        electrode_labels = np.array([''] * 256, dtype='S6')
+        for name in anat.dtype.names:
+            electrode_labels[anat[name][0, 0].flatten() - 1] = name
+        electrode_labels = np.array([word.decode("utf-8") for word in electrode_labels])
+        # electrode_labels = np.array([item[0][0] if len(item[0]) else '' for item in anatomy['electrodes'][0]])
+
+    elif elect_labels_filename:
+        a = scipy.io.loadmat(elect_labels_filename[0])
+        electrode_labels = np.array([elem[0] for elem in a['labels'][0]])
+
+    else:
+        electrode_labels = None
+        print('No electrode labels found')
+
+    return electrode_labels
 
 
 def main():
@@ -72,22 +118,77 @@ def main():
               rate=options.rate,vsmc=vsmc,ct=options.ct,sd=options.sd,\
               store=store,srf=options.srf)
 
-def transform(path,subject,block,rate=400.,vsmc=True,\
-              ct=87.75,sd=3.65,store=False,srf=1e4):
 
-    b_path = '%s/%s/%s_%s'%(path,subject,subject,block)
+def transform(path, subject, block, rate=400., vsmc=False,
+              cts=None, sds=None, srf=1e4):
+    """
+    Takes raw LFP data and does the standard hilb algorithm:
+    1) CAR
+    2) notch filters
+    3) Hilbert transform on different Gaussian bands
 
-    """
-    Load raw HTK files
-    """
-    rd_path = '%s/RawHTK'%b_path
-    HTKoutR = htk.readHTKs(rd_path)
-    X = HTKoutR['data']
+    Saves to os.path.join(b_path, subject + '_B' + block + '_Hilb.h5')
 
+    Parameters
+    ----------
+    path
+    subject
+    block
+    rate
+    vsmc
+    cts: filter center frequencies. If None, use Chang lab defaults
+    sds: filer standard deviations. If NOne, use Chang lab defaults
+    srf
+
+    Returns
+    -------
+    Nothing. The result is too big to have in memory and is saved filter-by-filter to a h5.
+
+
+
+    #ct=87.75,sd=3.65
     """
-    Downsample to 400 Hz
-    """
-    X = dse.downsampleEcog(X,rate,HTKoutR['sampling_rate']/srf)
+
+    ######## setup bandpass hilbert filter parameters
+    if cts is None:
+        fq_min = 4.
+        fq_max = 200.
+
+        scale = 7.
+        a = [np.log10(.39), .5]
+
+        cts = 2 ** (np.arange(np.log2(fq_min) * scale, np.log2(fq_max) * scale) / scale)
+
+        if sds is None:
+            sds = -a[0] * cts ** a[1]
+    ########
+
+
+
+    block = str(block)
+    s_path = os.path.join(path, subject)
+    b_path = os.path.join(s_path, subject + '_B' + block)
+    #b_path = '%s/%s/%s_%s'%(path,subject,subject,block)
+
+    # first, look for downsampled ECoG in block folder
+    ds_ecog_path = os.path.join(b_path, 'ecog400', 'ecog.mat')
+    if os.path.isfile(ds_ecog_path):
+        print('loading ecog')
+        with h5py.File(ds_ecog_path, 'r') as f:
+            X = f['ecogDS']['data'][:].T.astype('float32')
+            fs = f['ecogDS']['sampFreq'][0]
+    else:
+        """
+        Load raw HTK files
+        """
+        rd_path = '%s/RawHTK'%b_path
+        HTKoutR = htk.readHTKs(rd_path)
+        X = HTKoutR['data']
+
+        """
+        Downsample to 400 Hz
+        """
+        X = dse.downsampleEcog(X,rate,HTKoutR['sampling_rate']/srf)
 
     """
     Subtract CAR
@@ -97,12 +198,12 @@ def transform(path,subject,block,rate=400.,vsmc=True,\
     """
     Select electrodes
     """
-    electrodes = loadmat('%s/%s/Anatomy/%s_anatomy.mat'%(path,subject,subject))
+    #electrodes = loadmat('%s/%s/Anatomy/%s_anatomy.mat'%(path,subject,subject))
+    labels = load_electrode_labels(s_path)
     if vsmc:
-        elects = np.hstack([electrodes['anatomy']['preCG'][0][0][0],\
-                            electrodes['anatomy']['postCG'][0][0][0]])-1
+        elects = np.where((labels == 'precentral') | (labels == 'postcentral'))[0]
     else:
-        elecs = electrodes-1
+        elects = range(256)
     badElects = np.loadtxt('/%s/Artifacts/badChannels.txt'%b_path)-1
     elects = np.setdiff1d(elects,badElects)
 
@@ -118,27 +219,34 @@ def transform(path,subject,block,rate=400.,vsmc=True,\
     """
     Apply Notch filters
     """
-    X = notch.applyLineNoiseNotch(X,rate)
+    X = notch.applyLineNoiseNotch(X, rate)
 
     """
-    Apply Hilbert transform
-    """
-    Xas = aht.applyHilbertTransform(X,rate,ct,sd)
-
-    """
-    Store the results
+    Apply Hilbert transform and store
     """
 
-    if store:
-        with h5py.File('%s/pcsd_data/%s_%s_AS_%.1f_%.1f.h5'%(b_path,subject,block,ct,sd)) as f:
-            f.attrs['sampling_rate'] = rate
-            f.attrs['hilb_ct'] = ct
-            f.attrs['hilb_sd'] = sd
-            f.create_dataset(name='X',data=X,compression='gzip')
-            f.create_dataset(name='X_imag',data=Xas.imag,compression='gzip')
-            f.create_dataset(name='X_real',data=Xas.real,compression='gzip')
-    else:
-        return X,Xas
+    hilb_path = os.path.join(b_path, subject + '_B' + block + '_Hilb.h5')
+    with h5py.File(hilb_path, 'w') as f:
+        dset_real = f.create_dataset('X_real', (len(cts), X.shape[0], X.shape[1]), 'float32', compression="gzip")
+        dset_imag = f.create_dataset('X_imag', (len(cts), X.shape[0], X.shape[1]), 'float32', compression="gzip")
+        for i, (ct, sd) in enumerate(tqdm(zip(cts, sds), 'applying Hilbert transform', total=len(cts))):
+            dat = aht.applyHilbertTransform(X, rate, ct, sd)
+            dset_real[i] = dat.real.astype('float32')
+            dset_imag[i] = dat.imag.astype('float32')
+
+        for dset in (dset_real, dset_imag):
+
+            dset.dims[0].label = 'filter'
+            for val, name in ((cts, 'filter_center'), (sds, 'filter_sigma')):
+                if name not in f.keys():
+                    f[name] = val
+                dset.dims.create_scale(f[name], name)
+                dset.dims[0].attach_scale(f[name])
+
+            dset.dims[1].label = 'channel'
+            dset.dims[2].label = 'time'
+        f.attrs['sampling_rate'] = rate
+
 
 
 if __name__=='__main__':
