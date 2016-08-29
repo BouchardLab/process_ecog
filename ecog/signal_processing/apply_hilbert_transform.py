e0424640c094fab63c0f4d0bf20f5c47e1cf9924
JesseLivezey <jesse.livezey@gmail.com>
2016-08-29 11:45:27 -0700

    more rearranging

--- ecog/signal_processing/apply_hilbert_transform.py
+++ ecog/signal_processing/apply_hilbert_transform.py
@@ -1,44 +1,53 @@
 from __future__ import division
 import numpy as np
-from pyfftw.interfaces.numpy_fft import fft,ifft,fftfreq
+from pyfftw.interfaces.numpy_fft import fft, ifft, fftfreq
 
 __authors__ = "Alex Bujan"
 
 
-def applyHilbertTransform(X, rate, center, sd):
-    """Apply bandpass filtering with Hilbert transform using a Gaussian kernel
+def apply_hilbert_transform(X, rate, center=None, sd=None, kernel=None):
+    """
+    Apply bandpass filtering with Hilbert transform using a Gaussian kernel
+    or prespecified kernel.
     
     Parameters
     ----------
-    X   : array
-            Input data, dimensions (n_channels, n_timePoints)
-
+    X : array
+        Input data, dimensions (n_channels, n_timePoints)
     rate : float
-            Number of samples per second
-
+        Number of samples per second
     center : float
-            Center frequency of the Gaussian kernel
-    
-    sd  : float,
-            Standard deviation of the Gaussian kernel
-    .
+        Center frequency of the Gaussian kernel
+    sd : float,
+        Standard deviation of the Gaussian kernel
+
     Returns
     -------
-
-    Xc   : array
-            Bandpassed analytical signal (dtype: complex)
-
+    Xc : array
+        Bandpassed analytical signal (dtype: complex)
     """
-    #frequencies
-    T = X.shape[-1]
-    freq = fftfreq(T,1/rate)
-    #heaviside kernel
+
+    if kernel is None:
+        assert center is not None
+        assert sd is not None
+    else:
+        assert center is None
+        assert sd is None
+
+    # frequencies
+    time_samples = X.shape[-1]
+    freq = fftfreq(time_samples, 1./rate)
+    # heaviside kernel
     h = np.zeros(len(freq))
     h[freq>0]=2.
     h[0]=1.
-    #bandpass transfer function
-    k  = np.exp((-(np.abs(freq)-center)**2)/(2*(sd**2)))
-    #compute analytical signal
+    if kernel is None:
+        # bandpass transfer function
+        k  = np.exp((-(np.abs(freq)-center)**2)/(2*(sd**2)))
+    else:
+        k = kernel
+
+    # compute analytical signal
     Xc = ifft(fft(X)*h*k)
 
     return Xc
