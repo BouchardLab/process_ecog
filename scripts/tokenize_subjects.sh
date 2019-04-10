5c0fb7f80fe37179e0a55599d0006bb80becbf2d
Jesse Livezey <jlivezey@lbl.gov>
2019-04-10 16:08:52 -0700

    update tokenize script

--- scripts/tokenize_subjects.sh
+++ scripts/tokenize_subjects.sh
@@ -1,7 +1,7 @@
-python -u tokenize_data.py /project/projectdirs/m2043/jlivezey/EC2 1 8 9 15 76 89 105 --zscore 'silence' --data_type AA_avg
+python -u tokenize_data.py $HOME/data/EC2 1 8 9 15 76 89 105 --zscore 'silence' --data_type AA_avg
 
-python -u tokenize_data.py /project/projectdirs/m2043/jlivezey/EC9 15 39 46 49 53 60 63 --zscore 'silence' --data_type AA_avg
+python -u tokenize_data.py $HOM/data/EC9 15 39 46 49 53 60 63 --zscore 'silence' --data_type AA_avg
 
-python -u tokenize_data.py /project/projectdirs/m2043/jlivezey/GP31 1 2 4 6 9 21 63 65 67 69 71 78 82 83 --zscore 'silence' --data_type AA_avg
+python -u tokenize_data.py $HOME/data/GP31 1 2 4 6 9 21 63 65 67 69 71 78 82 83 --zscore 'silence' --data_type AA_avg
 
-python -u tokenize_data.py /project/projectdirs/m2043/jlivezey/GP33 1 5 30 --zscore 'silence' --data_type AA_avg
+python -u tokenize_data.py $HOME/data/GP33 1 5 30 --zscore 'silence' --data_type AA_avg
