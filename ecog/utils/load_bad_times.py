ab5e5c4442f38526f641986037b02ccc70cd0184
Jesse Livezey <jlivezey@lbl.gov>
2019-04-10 16:32:19 -0700

    fixed empty bad_times

--- ecog/utils/load_bad_times.py
+++ ecog/utils/load_bad_times.py
@@ -18,8 +18,11 @@ def load_bad_times(nwb):
     bad_times : ndarray, (n_windows, 2)
         Pairs of start and stop times for bad segments.
     """
-    start = nwb.invalid_times['start_time'].data[:]
-    stop = nwb.invalid_times['stop_time'].data[:]
-
-    bad_times = np.stack([start, stop], axis=1)
+    times = nwb.invalid_times
+    if times is not None:
+        start = nwb.invalid_times['start_time'].data[:]
+        stop = nwb.invalid_times['stop_time'].data[:]
+        bad_times = np.stack([start, stop], axis=1)
+    else:
+        bad_times = np.array([])
     return bad_times
